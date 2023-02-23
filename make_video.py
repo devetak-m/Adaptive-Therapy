653c7a8390335a0039a6e5c57971d171baff3278
Ben Mindlin <bennm37@gmail.com>
2023-02-23 14:57:58 +0000

    Minor make video changes

--- make_video.py
+++ make_video.py
@@ -138,34 +138,6 @@ anim.save("media/continuous_1.mp4")
 fig, ax, anim = model3.animate_cells_graph(stride=10,interval=80)
 anim.save("media/adaptive_2.mp4")
 
-fig, ax = plt.subplots()
-fig, ax, anim = model1.animate_cells((fig, ax))
-anim.save("media/adaptive_1.mp4")
-
-fig, ax = plt.subplots()
-fig, ax, anim = model2.animate_cells((fig, ax))
-anim.save("media/continuous_1.mp4")
-
-fig, ax = plt.subplots()
-fig, ax, anim = model3.animate_cells((fig, ax))
-anim.save("media/adaptive_2.mp4")
-
-fig, ax = plt.subplots()
-fig, ax, anim = model1.animate_graph((fig, ax))
-anim.save("media/adaptive_1.mp4")
-
-fig, ax = plt.subplots()
-fig, ax, anim = model2.animate_graph((fig, ax))
-anim.save("media/continuous_1.mp4")
-
-fig, ax = plt.subplots()
-fig, ax, anim = model3.animate_graph((fig, ax))
-anim.save("media/adaptive_2.mp4")
-
-
-
-# fig, ax, anim = model.animate_cells_graph(stride=10,interval=80)
-# anim.save("media/nice_abm.mp4")
 
 #     fig,ax = plt.subplots()
 #     fig,ax,anim = model.animate_cells((fig,ax))
